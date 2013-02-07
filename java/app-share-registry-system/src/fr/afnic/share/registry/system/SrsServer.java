7817b86fb969c41124a75f3574e4d059913ae275
Razmoket <julien.alaphilippe@gmail.com>
2013-02-07 17:45:21 +0100

    ajout fichier

---
+++ java/app-share-registry-system/src/fr/afnic/share/registry/system/SrsServer.java
@@ -0,0 +1,13 @@
+package fr.afnic.share.registry.system;
+
+public class SrsServer {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
